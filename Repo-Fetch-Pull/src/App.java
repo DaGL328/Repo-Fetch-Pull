47ace763757d06947e3d1e6beaf144a3c7307bc9
DaGL328 <dgamardo21223@iesjoanramis.org>
2026-01-22 17:33:10 +0100

    Cambio realizado desde GitHub

--- Repo-Fetch-Pull/src/App.java
+++ Repo-Fetch-Pull/src/App.java
@@ -1,6 +1,6 @@
 public class App {
 	
     public static void main(String[] args) {
-        System.out.println("Aplicación iniciada");
+        System.out.println("Mensaje modificado desde GitHub");
     }
 } 
